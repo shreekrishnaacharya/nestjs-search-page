b5f665055db366f54c7e07326081a8ce0517d925
krishna.ac18 <krishna.acharya@rewasoft.com.np>
2024-04-16 17:16:25 +0545

    adding

--- publish/models/entity.d.ts
+++ publish/models/entity.d.ts
@@ -1,6 +1,6 @@
 import { FindManyOptions } from "typeorm";
 import { Page } from "../models/page.model";
-import { IFindAllByPage, IFindOne } from "../interfaces";
+import { IFindAllByPage, IFindOne, IFindOptionByPage } from "../interfaces";
 export declare function findAllByPage<T>({ repo, page, queryDto, customQuery }: IFindAllByPage): Promise<Page<T>>;
-export declare function findOptions<T>({ page, queryDto, customQuery }: IFindAllByPage): Promise<FindManyOptions>;
+export declare function findOptions<T>({ page, queryDto, customQuery }: IFindOptionByPage): Promise<FindManyOptions>;
 export declare function findOne<T>({ id, repo, queryDto, customQuery }: IFindOne): Promise<T>;
